444b24551cd6c86751d435909c7eff69f49e108d
yongheuicho <yongheuicho@gmail.com>
2018-04-17 11:34:06 +0900

    [2018-04-17] TestNode #5

--- comtPush.bat
+++ comtPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-04-17] TestNode #4"
+git commit -m "[2018-04-17] TestNode #5"
 git push -u origin master
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-console.log('Hello, world!');
-
-const circle = require('./circle');
-console.log('Area of a circle = ' + circle.area(3));
-console.log('Circumference of a circle = ' + circle.circum(3));
-console.log('Pi = ' + circle.pi);
-
-const square = require('./square');
-let mySquare = square(5);
-mySquare.width = 10;
-console.log('Area of a square = ' + mySquare.area());
-console.log('Circumference of a square = ' + mySquare.circum());
+const http = require('http');
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('Hello, world!');
+    res.end();
+});
+server.listen(8000, 'localhost');
+console.log('Server is running at localhost:8000');
