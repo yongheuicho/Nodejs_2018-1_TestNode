2f287d58ca0d5bf2af247914971b5a4b4f343e88
yongheuicho <yongheuicho@gmail.com>
2018-04-17 10:17:54 +0900

    [2018-04-17] TestNode #1

---
+++ comtPush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "[2018-04-17] TestNode #1"
+git push -u origin master
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('Hello, world!');
+console.log('Node.js version: ' + process.version);
+console.log(process.memoryUsage());
+console.log(process.env);
