8089cc9df49d98fc86f5686686ffe5f6c5d94454
yongheuicho <yongheuicho@gmail.com>
2018-04-17 10:56:36 +0900

    [2018-04-17] TestNode #3

--- comtPush.bat
+++ comtPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-04-17] TestNode #2"
+git commit -m "[2018-04-17] TestNode #3"
 git push -u origin master
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log('Hello, world!');
+
 const circle = require('./circle');
 console.log('Area of a circle = ' + circle.area(3));
 console.log('Circumference of a circle = ' + circle.circum(3));
 console.log('Pi = ' + circle.pi);
+
+const square = require('./square');
+let mySquare = square(4);
+console.log('Area of a square = ' + mySquare.area());
